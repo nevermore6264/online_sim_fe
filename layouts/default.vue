7392802f3889d64b8d17a73ac75614459d5fc41f
David <nevermore6264@example.com>
2024-12-28 04:27:30 +0700

    first commit

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,18 +7,3 @@
     <Footer />
   </div>
 </template>
-
-<script setup>
-import { ref, onMounted } from "vue";
-
-const isAuthenticated = ref(false);
-const isLoading = ref(true);
-
-onMounted(() => {
-  const token = localStorage.getItem("token");
-  isAuthenticated.value = token !== null;
-  isLoading.value = false;
-});
-</script>
-
-<style scoped></style>
--- layouts/landing.vue
+++ layouts/landing.vue
@@ -8,17 +8,4 @@
   </div>
 </template>
 
-<script setup>
-import { ref, onMounted } from "vue";
-
-const isAuthenticated = ref(false);
-const isLoading = ref(true);
-
-onMounted(() => {
-  const token = localStorage.getItem("token");
-  isAuthenticated.value = token !== null;
-  isLoading.value = false;
-});
-</script>
-
-<style scoped></style>
+<script setup></script>
