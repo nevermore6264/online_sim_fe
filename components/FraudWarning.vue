1d52beab02053063f052bb33c13e92e2864adbfb
David <nevermore6264@example.com>
2024-12-31 04:57:08 +0700

    add

--- components/FraudWarning.vue
+++ components/FraudWarning.vue
@@ -2,7 +2,7 @@
   <div class="fraud-warning">
     <p>
       There are fraudulent sites that copy our design. Check the site domain
-      when you visit the site. Our domain is <strong>SMSPVA.COM</strong>. Be
+      when you visit the site. Our domain is <strong>Verifysms.com</strong>. Be
       careful and do not trust fraudulent sites. We are not responsible for the
       actions of these fraudulent sites.
     </p>
