9b95b70a392ee2fc8ffd67a7320a2a4e5596ebed
David <nevermore6264@example.com>
2025-01-13 22:50:28 +0700

    add

--- components/API.vue
+++ components/API.vue
@@ -15,74 +15,98 @@ import { ref } from "vue";
 
 const faqItems = ref([
   {
-    question: "For what purposes is it forbidden to use our proxies?",
+    question: "General",
     answer: `
-      <ul>
-        <li>Fraud, burglaries, insults, threats, and slander;</li>
-        <li>Password selection (brute force), scanning, and port vulnerability checks;</li>
-        <li>Creating phishing sites;</li>
-        <li>Spam (including on forums, websites, and blogs), or any activity leading to IP blacklists;</li>
-        <li>Email spamming;</li>
-        <li>Spreading malware (viruses, trojans, etc.);</li>
-        <li>Hacking websites or searching for vulnerabilities (e.g., SQL injection);</li>
-        <li>Distributing copyrighted materials without permission (e.g., videos, music, software);</li>
-        <li>Violating laws of the server's hosting country.</li>
-      </ul>
+      <pre>
+Only GET requests are accepted.
+The server always responds in JSON format. If the request was successful, the status field will be 1; otherwise, 0. If status = 0, the error cause will be indicated in the msg field.
+Most requests require the user's API key to be specified.
+      </pre>
     `,
     open: false,
   },
   {
-    question: "What protocols do your proxies support?",
-    answer:
-      "Our proxies support HTTP, HTTPS, and SOCKS5 protocols. Full details are provided after purchase.",
-    open: false,
-  },
-  {
-    question: "Can I get a refund for a proxy?",
-    answer:
-      "A refund can only be provided if the proxy is not working. If the proxies are technically functional, refunds will not be issued.",
-    open: false,
-  },
-  {
-    question: "How to set up a proxy in an Android device?",
+    question: "List of available countries",
     answer: `
-      <ol>
-        <li>Open your Android's Settings.</li>
-        <li>Tap Wi-Fi.</li>
-        <li>Tap and hold the Wi-Fi network name.</li>
-        <li>Select Modify Network.</li>
-        <li>Click Advanced Options.</li>
-        <li>Tap Manual.</li>
-        <li>Enter the proxy's hostname and port (e.g., 127.0.0.1:10101).</li>
-        <li>Tap Save.</li>
-      </ol>
+      <h4>Request</h4>
+      <pre>https://smspva.com/api/rent.php?method=getcountries</pre>
+      <h4>Response</h4>
+      <pre>{
+  "status": 1,
+  "data": [
+    {
+      "name": "Россия", // country name
+      "code": "RU"      // country code
+    }
+  ]
+}</pre>
     `,
     open: false,
   },
   {
-    question: "How to set up a proxy on an iPhone?",
+    question: "List of available services: count, price, etc.",
     answer: `
-      <ol>
-        <li>Open your iPhone settings.</li>
-        <li>Tap Wi-Fi.</li>
-        <li>Select the info icon next to the Wi-Fi network.</li>
-        <li>Scroll to HTTP Proxy and select Configure Proxy.</li>
-        <li>Tap Manual and enter the server and port (e.g., us.smartproxy.com:10101).</li>
-        <li>Turn on authentication (if needed) and add your username and password.</li>
-        <li>Tap back to save your settings.</li>
-      </ol>
+      <h4>Request</h4>
+      <pre>https://smspva.com/api/rent.php?method=getdata&country=RU</pre>
+      <h4>Response</h4>
+      <pre>{
+  "status": 1,
+  "data": [
+    {
+      "name": "Airbnb",   // service name
+      "service": "opt46", // service code
+      "price_day": 6,     // price per day
+      "count": 359        // available count
+    }
+  ]
+}</pre>
     `,
     open: false,
   },
   {
-    question: "How to set up a proxy in Windows 10?",
+    question: "Rent number",
     answer: `
-      <ol>
-        <li>Click the Windows logo and search for "Change proxy settings".</li>
-        <li>In the Proxy settings window, scroll to "Manual proxy setup".</li>
-        <li>Turn on "Use a proxy server".</li>
-        <li>Enter the address and port, then click Save.</li>
-      </ol>
+      <h4>Request</h4>
+      <pre>https://smspva.com/api/rent.php?method=create&apikey=yourapikeyhere&dtype=week&dcount=1&country=RU&service=opt6</pre>
+      <h4>Response</h4>
+      <pre>{
+  "status": 1,
+  "data": {
+    "id": 40370,             // ID of order
+    "pnumber": "9096037108", // phone number without country code
+    "ccode": "+7",           // digital country code
+    "service": "opt6",       // service code
+    "until": 1587633960      // UNIX timestamp of expire date
+  }
+}</pre>
+    `,
+    open: false,
+  },
+  {
+    question: "Add another service to order",
+    answer: `
+      <h4>Request</h4>
+      <pre>https://smspva.com/api/rent.php?method=orders&apikey=yourapikeyhere</pre>
+      <h4>Response</h4>
+      <pre>{
+  "status": 1,
+  "data": [
+    {
+      "id": 40300,              // ID of order
+      "scode": "opt16",         // service code
+      "sname": "Instagram",     // service name
+      "state": 0,               // state of number
+      "pnumber": "9096068511",  // phone number without country code
+      "ccode": "+7",            // digital country code
+      "cname": "RU",            // country code
+      "hasnewsms": false,       // flag of new SMS
+      "until": 1587552240,      // UNIX timestamp of expire date
+      "canprolong": true,       // flag of availability of prolongation
+      "canprolongmax": 162,     // max days for prolongation
+      "canprolonguntil": 1634883780 // Unix timestamp for max prolongation
+    }
+  ]
+}</pre>
     `,
     open: false,
   },
@@ -116,23 +140,6 @@ const toggleItem = (index) => {
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
 }
 
-.faq-title {
-  padding: 1rem;
-  margin: 0;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  cursor: pointer;
-  font-size: 1.1rem;
-  font-weight: bold;
-}
-
-.faq-toggle {
-  font-size: 1.5rem;
-  color: #888;
-  transition: transform 0.3s ease;
-}
-
 .faq-content {
   padding: 1rem;
   background-color: rgb(245, 245, 245);
@@ -141,31 +148,10 @@ const toggleItem = (index) => {
   color: #555;
 }
 
-.faq-content ul {
-  list-style-type: disc;
-}
-
-.faq-content ol {
-  list-style-type: decimal;
-}
-
-.faq-content ul li {
-  margin-bottom: 0.5rem;
-  line-height: 20px;
-  font-size: 14px;
+.faq-content pre {
+  background-color: #f5f5f5;
   padding: 10px;
-}
-
-.faq-content p {
-  margin: 0;
-}
-
-.faq-content a {
-  color: #007bff;
-  text-decoration: none;
-}
-
-.faq-content a:hover {
-  text-decoration: underline;
+  overflow-x: auto;
+  border-radius: 5px;
 }
 </style>
