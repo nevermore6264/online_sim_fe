dec54148c3fd5376ed22c5a56b85c4e763d833f5
David <nevermore6264@example.com>
2025-02-03 03:44:53 +0700

    Add

--- pages/news.vue
+++ pages/news.vue
@@ -5,22 +5,24 @@
     <DataView :value="products" paginator :rows="5">
       <template #list="slotProps">
         <div class="block">
-          <div v-for="(item, index) in slotProps.items" :key="index">
-            <div class="flex sm:flex-row sm:items-center p-6 gap-4 shortNews">
+          <div
+            v-for="(item, index) in slotProps.items"
+            :key="index"
+            class="shortNews"
+          >
+            <div class="flex sm:items-start p-1 gap-4">
               <div class="flex justify-between md:items-center flex-1 gap-6">
                 <div class="flex flex-row justify-between items-start gap-2">
                   <div>
                     <span class="text-lg font-medium mt-2">
-                      <!-- {{ item.text }} -->
                       Added Provider Selection in the Rental Section!
                     </span>
                     <div
                       class="font-medium text-surface-500 dark:text-surface-400 text-sm"
                     >
-                      <!-- {{ item.text }} -->
                       Dear users! We have added the ability to select a provider
                       for the desired country in the rental section! If you need
-                      a specific provider, you can choose it after selectin
+                      a specific provider, you can choose it after selecting.
                     </div>
                   </div>
                 </div>
@@ -32,16 +34,14 @@
                   :alt="item.text"
                 />
               </div>
-              <div class="flex flex-col md:items-end gap-8">
-                <div class="flex flex-row-reverse md:flex-row gap-2">
-                  <Button
-                    icon="pi pi-arrow-right"
-                    label="Read more"
-                    iconPos="right"
-                    class="flex-auto md:flex-initial whitespace-nowrap"
-                  ></Button>
-                </div>
-              </div>
+            </div>
+            <div class="flex flex-col md:items-end gap-2 mt-2">
+              <Button
+                icon="pi pi-arrow-right"
+                label="Read more"
+                iconPos="right"
+                class="flex-auto md:flex-initial whitespace-nowrap"
+              ></Button>
             </div>
           </div>
         </div>
@@ -88,12 +88,15 @@ export default {
 .shortNews {
   width: 100%;
   max-width: 700px;
-  height: 200px;
   border: 0px;
   background-color: rgb(245, 245, 245);
   border-radius: 10px;
   box-shadow: 0px 0px 0px 0px rgb(255, 255, 255);
   padding: 10px;
   margin: 0 auto 20px;
+}
+
+.p-dataview .p-dataview-content {
+  background: linear-gradient(to right, #d7f2fe, #ffffff) !important;
 }
 </style>
