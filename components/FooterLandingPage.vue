fe1ba74120f14aec39e776a7d4de21808f40eff2
David <nevermore6264@example.com>
2025-02-24 10:41:17 +0700

    Add

--- components/FooterLandingPage.vue
+++ components/FooterLandingPage.vue
@@ -6,10 +6,23 @@
         <!-- Thông tin công ty -->
         <div class="footer-text-box">
           <p class="footer-text">
-            Musashino, Tokyo | Registration code: 180-0006 |
+            <a
+              href="https://www.google.com/maps/place/3-ch%C5%8Dme-15+Nakach%C5%8D,+Musashino,+Tokyo+180-0006,+Nh%E1%BA%ADt+B%E1%BA%A3n/@35.7081548,139.5656352,17z/data=!3m1!4b1!4m6!3m5!1s0x6018efadae409857:0x9a8c1e82a73deedd!8m2!3d35.7081261!4d139.5657168!16s%2Fg%2F11bw4cwzsk?entry=ttu&g_ep=EgoyMDI1MDIxOS4xIKXMDSoASAFQAw%3D%3D"
+              target="_blank"
+              rel="noopener noreferrer"
+              >Musashino, Tokyo | Registration code: 180-0006 |</a
+            >
             <nuxt-link to="/">Public Offer, Terms and Policy</nuxt-link>
           </p>
-          <p class="footer-address">3-chōme-15 Nakachō</p>
+          <p class="footer-address">
+            <a
+              href="https://www.google.com/maps/place/3-ch%C5%8Dme-15+Nakach%C5%8D,+Musashino,+Tokyo+180-0006,+Nh%E1%BA%ADt+B%E1%BA%A3n/@35.7081548,139.5656352,17z/data=!3m1!4b1!4m6!3m5!1s0x6018efadae409857:0x9a8c1e82a73deedd!8m2!3d35.7081261!4d139.5657168!16s%2Fg%2F11bw4cwzsk?entry=ttu&g_ep=EgoyMDI1MDIxOS4xIKXMDSoASAFQAw%3D%3D"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              3-chōme-15 Nakachō
+            </a>
+          </p>
         </div>
       </div>
     </div>
@@ -51,5 +64,17 @@
   margin: 5px 0;
   font-size: 12px;
   line-height: 14px;
+}
+
+.footer-text-box {
+  text-align: center; /* Đảm bảo thông tin được căn giữa */
+}
+
+.footer-text-box a {
+  color: rgb(99, 99, 99);
+  margin: 5px 0;
+  font-size: 12px;
+  line-height: 14px;
+  text-decoration: none;
 }
 </style>
